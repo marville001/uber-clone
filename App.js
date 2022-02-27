0b52730d6781abfaa7cce72c67b15345c3bceab5
Martin Mwangi <mwangimartin1904@gmail.com>
2022-02-27 22:57:01 +0300

    Keyboard avoiding view

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { LogBox } from "react-native";
+import { LogBox, KeyboardAvoidingView, Platform } from "react-native";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
@@ -9,8 +9,8 @@ import { createStackNavigator } from "@react-navigation/stack";
 import MapScreen from "./screens/MapScreen";
 
 LogBox.ignoreLogs([
-    "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
-  ]);
+    "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+]);
 
 export default function App() {
     const Stack = createStackNavigator();
@@ -19,25 +19,30 @@ export default function App() {
         <Provider store={store}>
             <NavigationContainer>
                 <SafeAreaProvider>
-                    <Stack.Navigator>
-                        <Stack.Screen
-                            name="HomeScreen"
-                            component={HomeScreen}
-                            options={{
-                                headerShown: false,
-                            }}
-                        />
-                        <Stack.Screen
-                            name="MapScreen"
-                            component={MapScreen}
-                            options={{
-                                headerShown: false,
-                            }}
-                        />
-                    </Stack.Navigator>
+                    <KeyboardAvoidingView
+                        behavior={Platform.OS === "ios" ? "padding" : "height"}
+                        style={{ flex: 1 }}
+                        keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+                    >
+                        <Stack.Navigator>
+                            <Stack.Screen
+                                name="HomeScreen"
+                                component={HomeScreen}
+                                options={{
+                                    headerShown: false,
+                                }}
+                            />
+                            <Stack.Screen
+                                name="MapScreen"
+                                component={MapScreen}
+                                options={{
+                                    headerShown: false,
+                                }}
+                            />
+                        </Stack.Navigator>
+                    </KeyboardAvoidingView>
                 </SafeAreaProvider>
             </NavigationContainer>
         </Provider>
     );
 }
-
