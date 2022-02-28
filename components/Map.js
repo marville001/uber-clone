fefeb42035ff7003471abcc82db3bb39587e34a2
Martin Mwangi <mwangimartin1904@gmail.com>
2022-02-28 13:20:50 +0300

    Load distance metrics and updates

--- components/Map.js
+++ components/Map.js
@@ -2,8 +2,12 @@ import { StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useRef } from "react";
 import MapView, { Marker } from "react-native-maps";
 import tw from "tailwind-react-native-classnames";
-import { useSelector } from "react-redux";
-import { selectDestination, selectOrigin } from "../slices/navSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    selectDestination,
+    selectOrigin,
+    setTravelTimeInformation,
+} from "../slices/navSlice";
 import MapViewDirections from "react-native-maps-directions";
 import { GOOGLE_MAPS_KEY } from "@env";
 
@@ -12,6 +16,7 @@ const Map = () => {
     const destination = useSelector(selectDestination);
 
     const mapRef = useRef(null);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         if (!origin || !destination) return;
@@ -20,6 +25,26 @@ const Map = () => {
             edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
         });
     }, [origin, destination]);
+
+    useEffect(() => {
+        if (!origin || !destination) return;
+
+        const getTravelTime = async () => {
+            try {
+                const res = await fetch(
+                    `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin.description}&destinations=${destination.description}&units=imperial&key=${GOOGLE_MAPS_KEY}`
+                );
+
+                const data = await res.json();
+
+                dispatch(setTravelTimeInformation(data.rows[0].elements[0]));
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        getTravelTime();
+    }, [origin, destination, GOOGLE_MAPS_KEY]);
 
     return (
         <MapView
--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -39,62 +39,59 @@ const RideOptionsCard = () => {
 
     const navigation = useNavigation();
     return (
-        <ScrollView style={tw`bg-white flex-grow`}>
-            <>
-                <SafeAreaView style={tw`bg-white flex-grow pb-5`}>
-                    <View>
-                        <TouchableOpacity
-                            onPress={() => navigation.goBack()}
-                            style={tw`absolute top-3 left-5 p-3 rounded-full z-10`}
-                        >
-                            <Icon name="chevron-left" type="fontawesome" />
-                        </TouchableOpacity>
-                        <Text style={tw`text-center py-5 text-xl`}>
-                            Select a Ride
-                        </Text>
-                    </View>
-                    <FlatList
-                        data={data}
-                        keyExtractor={(item) => item.id}
-                        renderItem={({
-                            item: { id, image, title, multiplier },
-                            item,
-                        }) => (
-                            <TouchableOpacity
-                                onPress={() => setSelected(item)}
-                                style={tw`flex-row items-center justify-between px-10
+        <SafeAreaView style={tw`bg-white flex-grow`}>
+            <View>
+                <TouchableOpacity
+                    onPress={() => navigation.goBack()}
+                    style={tw`absolute top-3 left-5 p-1 rounded-full z-10`}
+                >
+                    <Icon name="chevron-left" type="fontawesome" />
+                </TouchableOpacity>
+                <Text style={tw`text-center py-3 text-xl`}>Select a Ride</Text>
+            </View>
+            <FlatList
+                data={data}
+                keyExtractor={(item) => item.id}
+                renderItem={({
+                    item: { id, image, title, multiplier },
+                    item,
+                }) => (
+                    <TouchableOpacity
+                        onPress={() => setSelected(item)}
+                        style={tw`flex-row items-center justify-between px-10
                         ${id === selected?.id && "bg-gray-200"}
                         `}
-                            >
-                                <Image
-                                    style={{
-                                        width: 100,
-                                        height: 100,
-                                        resizeMode: "contain",
-                                    }}
-                                    source={{ uri: image }}
-                                />
-                                <View style={tw`-ml-6`}>
-                                    <Text style={tw`text-xl font-semibold`}>
-                                        {title}
-                                    </Text>
-                                    <Text>Travel time ...</Text>
-                                </View>
-                                <Text style={tw`text-xl`}>$99</Text>
-                            </TouchableOpacity>
-                        )}
-                    />
-
-                    <View>
-                        <TouchableOpacity style={tw`bg-black py-3 m-3`}>
-                            <Text style={tw`text-center text-white text-xl`}>
-                                Choose {selected?.title}
+                    >
+                        <Image
+                            style={{
+                                width: 100,
+                                height: 100,
+                                resizeMode: "contain",
+                            }}
+                            source={{ uri: image }}
+                        />
+                        <View style={tw`-ml-6`}>
+                            <Text style={tw`text-xl font-semibold`}>
+                                {title}
                             </Text>
-                        </TouchableOpacity>
-                    </View>
-                </SafeAreaView>
-            </>
-        </ScrollView>
+                            <Text>Travel time ...</Text>
+                        </View>
+                        <Text style={tw`text-xl`}>$99</Text>
+                    </TouchableOpacity>
+                )}
+            />
+
+            <View>
+                <TouchableOpacity
+                    disabled={!selected}
+                    style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-500"}`}
+                >
+                    <Text style={tw`text-center text-white text-xl`}>
+                        Choose {selected?.title}
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     );
 };
 
